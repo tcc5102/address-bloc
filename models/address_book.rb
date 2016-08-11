92314541a83bb87e99aa786f0281dd48bb5c470d
Tyler Cooper <tyler.cooper91@gmail.com>
2016-08-11 16:52:35 -0400

    asmt-5 complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -7,10 +7,22 @@ class AddressBook
     @entries = []
   end
 
+  def delete_entry(name, phone_number, email)
+    delete_entry = nil
+
+    @entries.each do |entry|
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email
+        delete_entry = entry
+      end
+    end
+
+    @entries.delete(delete_entry)
+  end
+
   def add_entry(name, phone_number, email)
 
     index = 0
-    entries.each do |entry|
+    @entries.each do |entry|
 
       if name < entry.name
         break
@@ -18,6 +30,6 @@ class AddressBook
       index += 1
     end
 
-    entries.insert(index, Entry.new(name, phone_number, email))
+    @entries.insert(index, Entry.new(name, phone_number, email))
   end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -18,6 +18,23 @@ RSpec.describe AddressBook do
     end
   end
 
+  describe "#delete_entry" do
+    it "deletes only one entry from the address book" do
+      book = AddressBook.new
+      book.add_entry('Michael Johnson', '777.019.5555', 'michaeljohnson@michaeljohnson.com')
+
+      name = 'Ada Lovelace'
+      phone_number = '010.012.1815'
+      email = 'augusta.king@lovelace.com'
+      book.add_entry(name, phone_number, email)
+
+      expect(book.entries.size).to eq(2)
+      book.delete_entry(name, phone_number, email)
+      expect(book.entries.size).to eq(1)
+      expect(book.entries.first.name).to eq('Michael Johnson')
+    end
+  end
+
   describe "#add_entry" do
     it "adds only one entry to the address book" do
       book = AddressBook.new

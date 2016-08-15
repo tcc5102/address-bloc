e501c1b75e9a5884340d62e4267d274e1096faa4
Tyler Cooper <tyler.cooper91@gmail.com>
2016-08-15 11:08:21 -0400

    csv functioning

--- models/address_book.rb
+++ models/address_book.rb
@@ -8,16 +8,16 @@ class AddressBook
     @entries = []
   end
 
-  def delete_entry(name, phone_number, email)
-    delete_entry = nil
+  def delete_entry(email)
+    deleted_entry = nil
 
     @entries.each do |entry|
-      if name == entry.name && phone_number == entry.phone_number && email == entry.email
-        delete_entry = entry
+      if email == entry.email
+        deleted_entry = entry
       end
     end
 
-    @entries.delete(delete_entry)
+    @entries.delete(deleted_entry)
   end
 
   def add_entry(name, phone_number, email)
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe AddressBook do
       book.add_entry(name, phone_number, email)
 
       expect(book.entries.size).to eq(2)
-      book.delete_entry(name, phone_number, email)
+      book.delete_entry(email)
       expect(book.entries.size).to eq(1)
       expect(book.entries.first.name).to eq('Michael Johnson')
     end
@@ -102,30 +102,29 @@ RSpec.describe AddressBook do
   end
 
   describe "import from entries_2.csv" do
-    it "imports the correct number of entries" do
+    before do
       book.import_from_csv("entries_2.csv")
+    end
 
+    it "imports the correct number of entries" do
       expect(book.entries.size).to eq(3)
     end
 
     it "imports the 1st entry" do
-      book.import_from_csv("entries_2.csv")
       # check first entry
-      entry_one = book.entries[0]
+      entry_one = book.entries[2]
       check_entry(entry_one, "Tyler", "555-000-1111", "tyler@blocmail.com")
     end
 
     it "imports the 2nd entry" do
-      book.import_from_csv("entries_2.csv")
       # check second entry
-      entry_two = book.entries[1]
+      entry_two = book.entries[0]
       check_entry(entry_two, "Katherine", "555-999-2222", "katherine@blocmail.com")
     end
 
     it "imports the 3rd entry" do
-      book.import_from_csv("entries_2.csv")
       #  Check the third entry
-      entry_three = book.entries[2]
+      entry_three = book.entries[1]
       check_entry(entry_three, "Laura", "555-123-4567", "laura@blocmail.com")
     end
   end
